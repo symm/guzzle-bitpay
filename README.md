f8469f4b9c2e5fc537365d17e3f62ebf1d327c98
Gareth Jones <me@gazj.co.uk>
2014-02-14 23:35:36 +0000

    Updated the createInvoice and getInvoice methods to return an Invoice Model
    BC break

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 BitPay Client for PHP
 =====================
 
-![Build Status](https://api.travis-ci.org/symm/guzzle-bitpay.png)
+[![Build Status](https://travis-ci.org/symm/guzzle-bitpay.png?branch=master)](https://travis-ci.org/symm/guzzle-bitpay)
 
 Access the [BitPay API v0.3](https://bitpay.com/bitcoin-payment-gateway-api) in PHP using [Guzzle](http://guzzlephp.org).
 
@@ -9,7 +9,6 @@ Access the [BitPay API v0.3](https://bitpay.com/bitcoin-payment-gateway-api) in 
 
 Usage
 -----
-
     use Symm\BitpayClient\BitpayClient;
 
     $config = array(
@@ -17,12 +16,14 @@ Usage
     );
     $client = BitpayClient::factory($config);
 
-    $invoice = $client->createInvoice(array('price' => 3, 'currency' => 'BTC'));
-    $response = $client->getInvoice(array('id' => $invoice['id']));
+    $invoice  = $client->createInvoice(array('price' => 3, 'currency' => 'BTC'));
+    // Will return a Symm\BitpayClient\Model\Invoice
+
+    $response = $client->getInvoice(array('id' => $invoice->getId()));
+    // Will return a Symm\BitpayClient\Model\Invoice
 
 Available Actions
 -----------------
-
     $api->createInvoice()
     $api->getInvoice()
     $api->getRates()
---
+++ src/Symm/BitpayClient/Model/Invoice.php
@@ -0,0 +1,254 @@
+<?php
+
+namespace Symm\BitpayClient\Model;
+
+use Guzzle\Service\Command\OperationCommand;
+use Guzzle\Service\Command\ResponseClassInterface;
+
+/**
+ * Invoice
+ */
+class Invoice implements ResponseClassInterface
+{
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @var  string
+     */
+    protected $url;
+
+    /**
+     * @var string
+     */
+    protected $posData;
+
+    /**
+     * @var string
+     */
+    protected $status;
+
+    /**
+     * @var string
+     */
+    protected $btcPrice;
+
+    /**
+     * @var double
+     */
+    protected $price;
+
+    /**
+     * @var string
+     */
+    protected $currency;
+
+    /**
+     * @var \DateTime
+     */
+    protected $invoiceTime;
+
+    /**
+     * @var \DateTime
+     */
+    protected $expirationTime;
+
+    /**
+     * @var \DateTime
+     */
+    protected $currentTime;
+
+    const STATUS_NEW       = 'new';
+    const STATUS_PAID      = 'paid';
+    const STATUS_CONFIRMED = 'confirmed';
+    const STATUS_COMPLETE  = 'complete';
+    const STATUS_EXPIRED   = 'expired';
+    const STATUS_INVALID   = 'invalid';
+
+    /**
+     * The allowed invoice statuses
+     *
+     * @var array
+     */
+    public static $allowedStatuses = array(
+        self::STATUS_NEW,
+        self::STATUS_PAID,
+        self::STATUS_CONFIRMED,
+        self::STATUS_COMPLETE,
+        self::STATUS_EXPIRED,
+        self::STATUS_INVALID
+    );
+
+    /**
+     * Constructor
+     *
+     * @param string    $id
+     * @param string    $url
+     * @param string    $posData
+     * @param string    $status
+     * @param string    $btcPrice
+     * @param double    $price
+     * @param string    $currency
+     * @param \DateTime $invoiceTime
+     * @param \DateTime $expirationTime
+     * @param \DateTime $currentTime
+     */
+    public function __construct(
+        $id,
+        $url,
+        $posData,
+        $status,
+        $btcPrice,
+        $price,
+        $currency,
+        \DateTime $invoiceTime,
+        \DateTime $expirationTime,
+        \DateTime $currentTime
+    ) {
+
+        $this->id             = $id;
+        $this->url            = $url;
+        $this->posData        = $posData;
+        $this->status         = $status;
+        $this->btcPrice       = $btcPrice;
+        $this->price          = $price;
+        $this->currency       = $currency;
+        $this->invoiceTime    = $invoiceTime;
+        $this->expirationTime = $expirationTime;
+        $this->currentTime    = $currentTime;
+    }
+
+    /**
+     * Create a response model object from a completed command
+     *
+     * @param OperationCommand $command That serialized the request
+     *
+     * @return self
+     */
+    public static function fromCommand(OperationCommand $command)
+    {
+        $response = $command->getResponse();
+        $json     = $response->json();
+
+        return new self(
+            $json['id'],
+            $json['url'],
+            $json['posData'],
+            $json['status'],
+            $json['btcPrice'],
+            $json['price'],
+            $json['currency'],
+            \DateTime::createFromFormat('U', round($json['invoiceTime'] / 1000)),
+            \DateTime::createFromFormat('U', round($json['expirationTime'] / 1000)),
+            \DateTime::createFromFormat('U', round($json['currentTime'] / 1000))
+        );
+    }
+
+    public static function convertMillisecondsToDateTime($milliseconds)
+    {
+        $seconds = round($milliseconds / 1000);
+        return \DateTime::createFromFormat('U', $seconds);
+    }
+
+    /**
+     * Get the btcPrice of the Invoice
+     *
+     * @return string
+     */
+    public function getBtcPrice()
+    {
+        return $this->btcPrice;
+    }
+
+    /**
+     * Get the currency of the Invoice
+     *
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * Get the currentTime of the Invoice
+     *
+     * @return \DateTime
+     */
+    public function getCurrentTime()
+    {
+        return $this->currentTime;
+    }
+
+    /**
+     * Get the expirationTime of the Invoice
+     *
+     * @return \DateTime
+     */
+    public function getExpirationTime()
+    {
+        return $this->expirationTime;
+    }
+
+    /**
+     * Get the id of the Invoice
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the invoiceTime of the Invoice
+     *
+     * @return \DateTime
+     */
+    public function getInvoiceTime()
+    {
+        return $this->invoiceTime;
+    }
+
+    /**
+     * Get the posData of the Invoice
+     *
+     * @return string
+     */
+    public function getPosData()
+    {
+        return $this->posData;
+    }
+
+    /**
+     * Get the price of the Invoice
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Get the status of the Invoice
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Get the url of the Invoice
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
